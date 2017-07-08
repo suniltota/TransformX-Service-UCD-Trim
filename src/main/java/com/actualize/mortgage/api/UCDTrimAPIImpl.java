012959a1b70b5a4cf049295c93f8ce25f145d168
Shravan Kumar Boragala <shravan.boragala@compugain.com>
2017-07-08 20:36:55 +0530

    updated logs at api level

--- src/main/java/com/actualize/mortgage/api/UCDTrimAPIImpl.java
+++ src/main/java/com/actualize/mortgage/api/UCDTrimAPIImpl.java
@@ -7,6 +7,9 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +25,11 @@ import com.actualize.mortgage.service.impl.UCDXMLServiceImpl;
 @RequestMapping("actualize/transformx/services/ucd/")
 public class UCDTrimAPIImpl {
 	
+	private static final Logger LOG = LogManager.getLogger(UCDTrimAPIImpl.class);
+	
 	@RequestMapping(value = "{version}/trim", method = { RequestMethod.POST }, produces = "application/xml")
     public String generateUCDXml(@PathVariable String version, @RequestBody String xmldoc) throws Exception {
+		LOG.info("user "+SecurityContextHolder.getContext().getAuthentication().getName()+" used Service: MISMO XML to UCD XML");
 		UCDXMLServiceImpl ucdxmlServiceImpl = new UCDXMLServiceImpl();
 		InputStream inputXmlStream = new ByteArrayInputStream(xmldoc.getBytes(StandardCharsets.UTF_8));
 		return ucdxmlServiceImpl.createClosingDisclosureUCDXML(inputXmlStream);
@@ -31,6 +37,7 @@ public class UCDTrimAPIImpl {
 
 	@RequestMapping(value = "{version}/ping", method = { RequestMethod.GET })
     public String status(@PathVariable String version) throws Exception {
+		LOG.info("user "+SecurityContextHolder.getContext().getAuthentication().getName()+" used Service: ping to Trim Service");
 		return "The Service for generating UCD XML is running and ready to accept the requests";
     }
 }
